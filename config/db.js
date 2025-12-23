09f01b5e52e97ff7adfd6c346bb69013738b4539
IntanNurAini <intan.nuraini2005@gmail.com>
2025-12-23 11:02:17 +0700

    perbarui product

--- config/db.js
+++ config/db.js
@@ -1,15 +1,27 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'tubes_web'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
   if (err) throw err;
   console.log('Database connected');
+  
+  db.query(`
+    CREATE TABLE IF NOT EXISTS products (
+      kode_produk VARCHAR(20) PRIMARY KEY,
+      nama_produk VARCHAR(100),
+      jumlah INT,
+      status VARCHAR(20)
+    )
+  `, () => {
+    console.log('Table products ready');
+  });
 });
 
 module.exports = db;
